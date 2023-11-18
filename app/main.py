2f4026a9269c9b63ad70cbecf0793bb8680c7e1b
Чернышков Максим <maksim@cherny.sh>
2023-11-18 12:32:37 +0300

    создание тестовых данных

--- app/main.py
+++ app/main.py
@@ -2,10 +2,16 @@ import flet as ft
 from utils import *
 from components.MainView import MainView
 
+from core.database import engine
+from core.database import Base
+
 
 def main(page: ft.Page):
     page.title = "Мероприятия"
     page.theme_mode = ft.ThemeMode.LIGHT
+
+    Base.metadata.create_all(engine)
+    add_sample_data()
 
     main_view = MainView(page)
 
